lisptick/crewmate

commit b9798b8efbf90abb46be7c2dee928257fab09359

Use Arel scopes instead hashed conditions to fix sqlite specs

app/models/person.rb
@@ -13,7 +13,7 @@ class Person < ActiveRecord::Base
   after_create :log_create
   after_destroy :log_delete, :cleanup_after
 
-#  validates_uniqueness_of :user, :scope => :project
+  #  validates_uniqueness_of :user, :scope => :project
   validates_presence_of :user, :project   # Make sure they both exist and are set
   validates_inclusion_of :role, :in => 0..3
   validates_uniqueness_of :project_id, :scope => :user_id
@@ -23,17 +23,21 @@ class Person < ActiveRecord::Base
   ROLES = {:observer => 0, :commenter => 1, :participant => 2, :admin => 3}
   PERMISSIONS = [:view,:edit,:delete,:all]
 
-  scope :admins, :conditions => "role = #{ROLES[:admin]}"
-
-  scope :from_unarchived, :joins => :project,
-    :conditions => ['projects.archived = ?', false]
-
-  scope :by_login, lambda { |login|
-    {:include => :user, :conditions => {'users.login' => login}}
+  scope :admins, lambda{
+    where(:role => ROLES[:admin])
   }
 
-  scope :in_alphabetical_order, :include => :user, :order => 'users.first_name ASC'
+  scope :from_unarchived, lambda{
+    joins(:project).where(:projects => {:archived => false})
+  }
 
+  scope :by_login, lambda { |login|
+    includes(:user).where(:users => {:login => login})
+  }
+
+  scope :in_alphabetical_order, lambda {
+    includes(:user).order('users.first_name ASC')
+  }
 
   attr_accessible :role, :permissions
 
@@ -75,15 +79,12 @@ class Person < ActiveRecord::Base
 
   def self.user_names_from_projects(projects, current_user = nil)
     project_ids = Array.wrap(projects).map(&:id)
-    connection.select_rows(<<-SQL)
-      SELECT people.project_id, users.login, users.first_name, users.last_name, people.id, users.id
-      FROM people
-      INNER JOIN projects ON projects.id = people.project_id
-      INNER JOIN users ON users.id = people.user_id
-      WHERE people.project_id IN (#{project_ids.join(',')})
-        AND (people.deleted IS NULL OR people.deleted IS FALSE)
-      ORDER BY users.id = #{current_user.try(:id).to_i} DESC,users.login
-    SQL
+    current_user_id = current_user.try(:id).to_i
+
+    select("people.project_id, people.user_id, users.login, users.first_name, users.last_name, people.id, users.id")
+      .joins(:project).joins(:user)
+      .where("people.project_id IN (?) AND (people.deleted IS NULL OR people.deleted = ?)", project_ids, false)
+      .order("users.id = #{current_user_id} DESC, users.login")
   end
 
   def user

app/models/activity.rb
@@ -6,21 +6,19 @@ class Activity < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  scope :for_task_lists, :conditions => "target_type = 'TaskList' || target_type = 'Task' || comment_target_type = 'TaskList' || comment_target_type = 'Task'"
-  scope :for_conversations, :conditions => "target_type = 'Conversation' || comment_target_type = 'Conversation'"
-  scope :for_tasks, :conditions => "target_type = 'Task' || comment_target_type = 'Task'"
-  scope :in_targets, lambda {|targets| {:conditions => ["target_id IN (?) OR comment_target_id IN (?)", *(Array(targets).collect(&:id)*2)]}}
-
-  scope :latest, :order => 'id DESC', :limit => Teambox.config.activities_per_page
-
-  scope :in_projects, lambda { |projects| { :conditions => ["project_id IN (?)", Array(projects).collect(&:id) ] } }
-  scope :limit_per_page, :limit => Teambox.config.activities_per_page
-  scope :by_id, :order => 'id DESC'
-  scope :by_updated, :order => 'updated_at desc'
-  scope :threads, :conditions => "target_type != 'Comment'"
-  scope :before, lambda { |activity_id| { :conditions => ["id < ?", activity_id ] } }
-  scope :after, lambda { |activity_id| { :conditions => ["id > ?", activity_id ] } }
-  scope :from_user, lambda { |user| { :conditions => { :user_id => user.id } } }
+  scope :for_task_lists, lambda{ where("target_type = 'TaskList' OR target_type = 'Task' OR comment_target_type = 'TaskList' OR comment_target_type = 'Task'") }
+  scope :for_conversations, lambda{ where("target_type = 'Conversation' OR comment_target_type = 'Conversation'") }
+  scope :for_tasks, lambda{ where("target_type = ? OR comment_target_type = ?", 'Task', 'Task') }
+  scope :in_targets, lambda { |targets| where("target_id IN (?) OR comment_target_id IN (?)", Array(targets).collect(&:id), Array(targets).collect(&:id)) }
+  scope :latest, lambda{ limit_per_page.by_id }
+  scope :in_projects, lambda { |projects| where(:project_id => Array(projects).collect(&:id)) }
+  scope :limit_per_page, lambda{ limit(Teambox.config.activities_per_page) }
+  scope :by_id, lambda{ order('id DESC') }
+  scope :by_updated, lambda{ order('updated_at DESC') }
+  scope :threads, lambda{ where("target_type != 'Comment'") }
+  scope :before, lambda { |activity_id| where("id < ?", activity_id) }
+  scope :after, lambda { |activity_id| where("id > ?", activity_id) }
+  scope :from_user, lambda { |user| where(:user_id => user.id) }
 
   def self.log(project,target,action,creator_id)
     project_id = project.try(:id)

app/models/conversation/tasks.rb
@@ -51,6 +51,7 @@ class Conversation
       task.save
       self.reload.destroy
     end
+
     task
   end
 end

commit bb271325749006d93cf47cf35581408fd89f8520

Fixing username autocompletion bug introduced by b9798b8efbf90abb46be7c2dee928257fab09359

app/models/person.rb
@@ -81,10 +81,10 @@ class Person < ActiveRecord::Base
     project_ids = Array.wrap(projects).map(&:id)
     current_user_id = current_user.try(:id).to_i
 
-    select("people.project_id, people.user_id, users.login, users.first_name, users.last_name, people.id, users.id")
-      .joins(:project).joins(:user)
+    joins(:project).joins(:user)
       .where("people.project_id IN (?) AND (people.deleted IS NULL OR people.deleted = ?)", project_ids, false)
-      .order("users.id = #{current_user_id} DESC, users.login")
+      .order("users.id = #{current_user_id} DESC, users.login").map { |row| [row.project_id, row.user.login, row.user.first_name, row.user.last_name, row.id, row.user.id] }
+
   end
 
   def user